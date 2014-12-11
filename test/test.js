8a2b8d244c14979c5d618c06367d9aadc0470519
gwendoux <gwenael.doux@cncc.fr>
2014-12-11 12:29:12 +0100

    Added more test
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- test/test.js
+++ test/test.js
@@ -5,9 +5,14 @@ var decode = require('../modules/decode.js').decode;
 describe("CleanName", function() {
     it("must be a string", function(done) {
         var varTest = "Questionnaire%20de%20synth%c3%a8se%20du%20dossier%20du%20Co-cac%20%20%c3%a0%20publier";
+        var TestResult = "Questionnaire de synth%c3%a8se du dossier du Co-cac  %c3%a0 publier";
         var result = decode(varTest);
 
+        expect(result).not.to.be.null();
+
         result.must.be.a.string();
+
+        result.must.be(TestResult);
 
         done();
     });
