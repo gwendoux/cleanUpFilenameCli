7284191d7be1443622368dee46718104c9bca6d1
gwendoux <gwenael.doux@cncc.fr>
2014-12-11 15:31:12 +0100

    fixed test
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- test/test.js
+++ test/test.js
@@ -1,16 +1,15 @@
-var expect = require('must');
-
-var cleanup = require('../modules/cleanup.js').cleanup;
+var expect = require('must'),
+    cleanup = require('../modules/cleanup.js').cleanup;
 
 describe("CleanName", function() {
     it("must be a string", function(done) {
-        var varTest = "%CB%D5%C7%A7";
-        //var TestResult = "Questionnaire de synthèse du dossier du Co-cac à publier";
+        var varTest = "Questionnaire%20de%20synth%c3%a8se%20du%20dossier%20du%20Co-cac%20%c3%a0%20publier";
+        var TestResult = "questionnaire-de-synthese-du-dossier-du-co-cac-a-publier";
         var result = cleanup(varTest);
 
         expect(result).not.to.be.null();
 
-        //result.must.be.a.string();
+        result.must.be.a.string();
 
         result.must.be(result);
 
