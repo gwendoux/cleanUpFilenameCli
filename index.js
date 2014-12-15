20ad619e8e80359215a03a9538115e2baf0429d3
gwendoux <gwenael.doux@cncc.fr>
2014-12-15 16:21:12 +0100

    Added prompt
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- index.js
+++ index.js
@@ -3,16 +3,30 @@
 
 var program = require('commander'),
     pck = require('./package.json'),
-    cleanup = require('./modules/cleanup.js').cleanup;
+    cleanup = require('./modules/cleanup.js').cleanup,
+    prompt = require('prompt');
 
 program
   .version(pck.version)
   .option('-f, --filename [filename]', 'filename to clean up')
   .parse(process.argv);
 
+if(program.filename) {
+    var input = program.filename;
+    var output = cleanup(input);
 
-var input = program.filename;
+    console.log("Result: %s", output);
 
-var output = cleanup(input);
+}
 
-console.log("Result: %s", output);
+else {
+    prompt.start();
+    prompt.get(['filename'], function (err, result) {
+
+    var input = result.filename;
+    var output = cleanup(input);
+
+    console.log("Result: %s", output);
+  });
+}
+
