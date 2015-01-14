83ae14599f73d4a504b6971fcea7b3a500c62521
gwendoux <gwenael.doux@cncc.fr>
2015-01-14 13:42:37 +0100

    added function to remove doubling dashes
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- modules/cleanup.js
+++ modules/cleanup.js
@@ -5,7 +5,12 @@ var urlencode = require('urlencode'),
 
 module.exports.cleanup = function (val) {
     val = urlencode.decode(val, 'utf-8');
+    // remove doubling spaces
+    val = val.replace(/\s{2,}/g, ' ');
+    // replace spaces with dashes
     val = val.replace(/\s/g, "-");
+    // remove multidashes
+    val = val.replace(/-+$|(-)+/g, '$1');
     val = removeDiacritics(val);
     val = lowerCase(val);
     return val;
