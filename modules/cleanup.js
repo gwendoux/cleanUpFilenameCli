f11a7ecc4688fdd36c24298a1b15f4bdb57de7a0
gwendoux <gwenael.doux@cncc.fr>
2015-01-26 14:42:36 +0100

    remove dot and dash from cleanup
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- modules/cleanup.js
+++ modules/cleanup.js
@@ -7,11 +7,11 @@ exports.cleanup = cleanup;
 
 function cleanup (val) {
     val = urlencode.decode(val, 'utf-8');
-    val = removeDiacritics(val);
-    val = val.replace(/\s{2,}/g, ' ')
-             .replace(/[^\w\s]|_/g, "")
-             .replace(/\s/g, "-")
-             .replace(/-+$|(-)+/g, '$1');
+    val = removeDiacritics(val); //remove accentued characters
+    val = val.replace(/\s{2,}/g, ' ') //
+             .replace(/[^\w\s\.-]|_/g, "") // remove extra characters except. (., _ and -)
+             .replace(/\s/g, "-") // replace space with dash
+             .replace(/-+$|(-)+/g, '$1'); // remove multidashes
     val = lowerCase(val);
     return val;
 };
