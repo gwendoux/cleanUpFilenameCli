e1e4e1eab5f57d370014058186fee699db031176
gwendoux <gwenael.doux@cncc.fr>
2015-01-27 14:25:44 +0100

    added new function to rewrite in filesystem

--- modules/output.js
+++ modules/output.js
@@ -1,10 +1,23 @@
 var cleanup =  require('./cleanup').cleanup,
+    fs = require('fs'),
     cp = require('copy-paste').global();
 
 exports.outputClipboard = outputClipboard;
+exports.outputFileSystem = outputFileSystem;
 
 function outputClipboard(input) {
     var output = cleanup(input);
     cp.copy(output);
     console.log("Result: %s", output);
+}
+
+function outputFileSystem (input) {
+    var output = cleanup(input);
+    fs.rename(input, output, function(err) {
+        if(err) {
+            throw err;
+        } else {
+            console.log("Rewrite filename with %s", output);
+        }
+    });
 }
