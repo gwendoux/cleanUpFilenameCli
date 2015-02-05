ffb06bce89f943ab90a8ebd915525d06fb4b6fac
gwendoux <gwenael.doux@cncc.fr>
2015-02-05 10:20:37 +0100

    Added task listing
    Signed-off-by: gwendoux <gwenael.doux@cncc.fr>

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
+var taskListing = require('gulp-task-listing');
 
 gulp.task('test', function () {
     return gulp.src('test/test.js', {read: false})
@@ -12,3 +13,7 @@ gulp.task('lint', function() {
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('help', taskListing.withFilters(null, 'default'));
+
+gulp.task('default', ['help']);
