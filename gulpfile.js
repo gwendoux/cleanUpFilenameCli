ca5dfae7fb1f1aba1eea7ea913698026bf296592
gwendoux <gwenael.doux@cncc.fr>
2015-02-05 09:51:57 +0100

    Added jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 
 gulp.task('test', function () {
     return gulp.src('test/test.js', {read: false})
         .pipe(mocha());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['./modules/*.js', './test/*.js', './index.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
